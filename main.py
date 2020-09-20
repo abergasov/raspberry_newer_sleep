941dea96848e1dc68efdb3a200e455008aed5d2b
Alexander Bergasov <a.bergasov@city-mobil.ru>
2020-09-20 23:13:10 +0300

    simply

---
+++ main.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+import time
+from Xlib import X, display
+
+
+def move_mouse(a, b):
+    d = display.Display()
+    s = d.screen()
+    root = s.root
+    root.warp_pointer(a, b)
+    d.sync()
+
+
+for i in range(1, 4):
+    move_mouse(i * 100, i * 100)
+    time.sleep(2)
